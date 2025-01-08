adad862d6ad78659bbf0fba1a578820f865a8b39
ksl0718 <ksl0718@nate.com>
2025-01-08 20:36:49 +0900

    01.08 코드 정리

--- SalCap/src/main/resources/static/salaryCalculator.js
+++ SalCap/src/main/resources/static/salaryCalculator.js
@@ -159,22 +159,7 @@ document.getElementById('calculateButton').addEventListener('click', function ()
     }else{
         return;
     }
-
-
-        // 2024.12.21 계산시 결과 보여주기 추가 - 이순
-        // 결과 출력
-        if(employeeName === '' || wagePerMinute === NaN || dateInput === NaN || startTimeInput>=endTimeInput){
-            calculateFlag = false;
-            return;
-        }
-        if(calculateFlag){
-            document.getElementById('result').textContent =
-            `${employeeName}님의 총 근무 시간은 ${totalMinutesWorked}분이며, 총 급여는 ${totalSalary.toLocaleString()}원입니다.`;
-            result.style.display = 'block'; // 결과 보여줌
-        }else{
-            return;
-        }
-    });
+ });
 
 
 // ///////hidden input에 값 넣는 메소드
